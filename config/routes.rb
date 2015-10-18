c0eea79828d5f064161be6551d8ff60a0cdeb2a0
Vishal Zambre <vishal.zambre@weboniselab.com>
2015-10-19 02:12:57 +0530

    add missing admin routes for recover password if frontend is not available

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,10 @@ Spree::Core::Engine.add_routes do
         get '/login' => 'user_sessions#new', :as => :login
         post '/login' => 'user_sessions#create', :as => :create_new_session
         get '/logout' => 'user_sessions#destroy', :as => :logout
+        get '/password/recover' => 'user_passwords#new', as: :recover_password
+        post '/password/recover' => 'user_passwords#create', as: :reset_password
+        get '/password/change' => 'user_passwords#edit', as: :edit_password
+        put '/password/change' => 'user_passwords#update', as: :update_password
       end
     end
   end

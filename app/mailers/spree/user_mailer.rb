9beca6a9fe4eb045b74c6b252833b0363ca98e36
Vishal Zambre <vishal.zambre@weboniselab.com>
2015-10-19 02:30:34 +0530

    add reset password instruction for admin if frontend not available

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -6,6 +6,12 @@ module Spree
       mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
     end
 
+    def reset_admin_password_instructions(user, token, *args)
+      @edit_password_reset_url = spree.admin_edit_password_url(reset_password_token: token, host: Spree::Store.current.url)
+
+      mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])
+    end
+
     def confirmation_instructions(user, token, opts={})
       @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Store.current.url)
 
